887edc73cc0d85b0c51c07715dc8a947fc0994b2
Khoa <khoa130704@gmail.com>
2023-03-01 10:08:25 +0700

    git commit A.py

--- A.py
+++ A.py
@@ -1,5 +1,16 @@
-def main():
-    print("Hello World")
+import sqlite3
 
-if __name__ == '__main__':
-    main()
+def save_to_db(id, name, gpa, rank):
+    conn = sqlite3.connect('STUDENT.sqlite')
+    cur = conn.cursor()
+    try:
+        cur.executescript('''
+            create table student (ID TEXT, Name TEXT, gpa number, rank TEXT)
+        ''')
+    except:
+        pass
+    conn.commit()
+    conn.close()
+
+if  __name__ == '__main__':
+    save_to_db(13, 'Khoa', 8.0, 0)
