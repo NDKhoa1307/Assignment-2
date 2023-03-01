89e5a0c57c15e838f47a0bd46a9fa87696649567
Khoa <khoa130704@gmail.com>
2023-03-01 08:54:57 +0700

    Committed all files

--- A.py
+++ A.py
@@ -1,15 +1,1 @@
-num = int(input().strip())
-
-temp = 0
-while num:
-    if len(str(num)) == 1:
-        temp += num % 10
-        if len(str(temp)) == 1:
-            print(temp)
-            break
-        else:
-            num = temp
-            temp = 0
-    else:
-        temp += num % 10
-        num //= 10
+print("Hello World")
