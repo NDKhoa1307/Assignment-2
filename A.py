713edc0b00345f15b5ed96d06ddced4af123c04b
Khoa <khoa130704@gmail.com>
2023-03-01 10:56:42 +0700

    Template for progress test 2

--- A.py
+++ A.py
@@ -1,16 +1,5 @@
-import sqlite3
+def main():
+    pass
 
-def save_to_db(id, name, gpa, rank):
-    conn = sqlite3.connect('STUDENT.sqlite')
-    cur = conn.cursor()
-    try:
-        cur.executescript('''
-            create table student (ID TEXT, Name TEXT, gpa number, rank TEXT)
-        ''')
-    except:
-        pass
-    conn.commit()
-    conn.close()
-
-if  __name__ == '__main__':
-    save_to_db(13, 'Khoa', 8.0, 0)
+if __name__ == '__main__':
+    main()
