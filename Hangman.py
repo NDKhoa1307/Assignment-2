969da58f7acfda3b0982922d7331f11260f761b1
Khoa <khoa130704@gmail.com>
2023-03-02 18:45:15 +0700

    Đã thêm phần chào mừng người chơi

--- Hangman.py
+++ Hangman.py
@@ -1,7 +1,26 @@
-def main():
-    print("Testing git push")
+import os
+
+def play_the_game():
+    
     pass
 
+#Main function
+def main():
+    os.system('cls')
+    play_game = input("Welcome to Hangman, do you wish to play the game ?(y/n):").strip()
+    if not (play_game == 'n' or play_game == 'y'):
+        while True:
+            play_game = input("I'm sorry, but your input is not valid, please re-enter(Only enter either 'y' for yes and 'n' for no):").strip()
+            if play_game == 'n' or play_game == 'y':
+                break
+                
+    if play_game == 'y':
+        play_the_game()
+    os.system('cls')
+    print("Thank you for stopping by, have a good day!")
+    pass
+
+
+#Main program
 if __name__ == '__main__':
     main()
-    pass
